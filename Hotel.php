c1d71cac50f6a86fb8b0d18baababab3c04eabe9
Ivan Moroz <menvil.menvil@gmail.com>
2020-02-09 23:48:18 +0200

    Update Hotel.php

--- Hotel.php
+++ Hotel.php
@@ -67,8 +67,84 @@ interface Hotel
 	public function getRating();
 
 	/**
-	 * Method return array of rating object.
-	 * @return Rating
+	 * Method return header of description. (Stay in the heart of New York)
+	 * @return string
 	 */
-	public function getLocation();
+	public function getHeaderDescription();
+
+	/**
+	 * Method return description.
+	 * @return string
+	 */
+	public function getDescription();
+
+	/**
+	 * Method property highlights (Guests love etc...)
+	 * @return array
+	 */
+	public function getPropertyHightlights();
+
+	/**
+	 * Method return array of the room object for property
+	 * @return Room[]
+	 */
+	public function getRooms();
+
+	/**
+	 * Method return hotelier info. (An Inside Look at Hotel Edison Times Square)
+	 * @return string
+	 */
+	public function getHotelierInfo();
+
+	/**
+	 * Method return surroundings of the hotel. Multi nested level array with section name and surrounding name with distance
+	 * ['What's nearby' => ['Richard Rodgers Theatre'=>'0.1 km', 'Minskoff Theatre' => '0.2 km']]
+	 *
+	 * @return array
+	 */
+	public function getSurroundings();
+
+	/**
+	 * Method return attractions of the hotel.
+	 * @return Attraction[]
+	 */
+	public function getAttractions();
+
+	/**
+	 * Method return array of restaurants of the hotel
+	 * @return array
+	 */
+	public function getRestaurants();
+
+	/**
+	 * Method return facilities of the hotel. Multi nested level array with section name and facility name with options
+	 * ['Cleaning Services' =>
+	 * 		[
+	 * 			['name'=>'Dry cleaning', 'option'=>'paid'],
+	 * 			['name'=>'Ironing service', 'option'=>'free']
+	 * 		]
+	 * ]
+	 *
+	 * @return array
+	 */
+	public function getFacilities();
+
+
+	/**
+	 * Method return array of rules of the hotel
+	 * @return array
+	 */
+	public function getRules();
+
+	/**
+	 * Method return final notes of the hotel (The fine print);
+	 * @return string
+	 */
+	public function getNotes();
+
+	/**
+	 * Method return FAQs. Return array of FAQs of the hotel
+	 * @return Faq[]
+	 */
+	public function getFaqs();
 }
